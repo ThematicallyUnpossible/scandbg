24ba722d085f904c45d5e43ea2cbf2e32333c5ec
ThematicallyUnpossible <fernando_linux@proton.me>
2026-06-30 23:42:09 +0700

    now read memory maps

--- main.cpp
+++ main.cpp
@@ -3,11 +3,23 @@
 #include <sys/uio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <limits>
+
+struct MemoryBlueprint
+{
+    unsigned long long m_start_address;
+    unsigned long long m_end_address;
+    std::size_t m_size;
+};
+
 
 int main(int argc, char* argv[])
 {
     if(argc != 2){
-        std::cout << "Invalid usage. Expected : scandbg <PID>";
+        std::cerr << "Invalid usage. Expected : scandbg <PID>";
         return 1;
     }
 
@@ -25,31 +37,97 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    std::string address_string{};
+    std::string target_mem_path = "/proc/" + pid_string + "/maps";
 
-    std::cout << "Type the virtual address you want to read : ";
-    std::cin >> address_string;
+    std::ifstream target_mem_file(target_mem_path);
 
-    unsigned long long remote_pointer = std::stoull(address_string, nullptr, 16);
+    std::vector<MemoryBlueprint> target_mem_available{};
 
-    int target_int;
+    std::string target_mem_line;
 
-    struct iovec local_object;
-    local_object.iov_base = &target_int;
-    local_object.iov_len = sizeof(target_int);
+    while (std::getline(target_mem_file, target_mem_line)) {
 
-    struct iovec remote_object;
-    remote_object.iov_base = (void*)remote_pointer;
-    remote_object.iov_len = sizeof(target_int);
+        size_t dash_index = target_mem_line.find('-');
+        size_t first_space_index = target_mem_line.find(' ');
 
-    ssize_t bytes_read = process_vm_readv(pid_int, &local_object, 1, &remote_object, 1, 0);
-    if(bytes_read == -1){
-        std::perror("Unable to read int virtual memory space ");
+        if (dash_index == std::string::npos || first_space_index == std::string::npos) {
+            continue;
+        }
+
+        std::string start_hex = target_mem_line.substr(0, dash_index);
+
+        std::string end_hex = target_mem_line.substr(dash_index + 1, first_space_index - (dash_index + 1));
+
+        std::string perms = target_mem_line.substr(first_space_index + 1, 4);
+
+        if (perms[0] != 'r') {
+            continue;
+        }
+
+        MemoryBlueprint region;
+        region.m_start_address = std::stoull(start_hex, nullptr, 16);
+        region.m_end_address   = std::stoull(end_hex, nullptr, 16);
+        region.m_size  = region.m_end_address - region.m_start_address;
+
+        target_mem_available.push_back(region);
+    }
+
+    int value_to_find{};
+    while(true)
+    {
+        write(1, "\033[2J\033[1;1H", 7);
+        std::cout << "Enter value to find : ";
+
+        std::cin >> value_to_find;
+
+        if(std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+        break;
+    }
+
+    int target_fd = open(target_mem_path.c_str(), O_RDONLY);
+    if(target_fd == -1){
+        std::cerr << "Unable to open " + target_mem_path;
         return 1;
     }
 
-    std::cout << target_int;
+    for( auto region : target_mem_available )
+    {
+        for (unsigned long long current_addr = region.m_start_address;
+             current_addr <= region.m_end_address - sizeof(int);
+             current_addr += sizeof(int))
+        {
+            int local_int_buffer = 0;
+
+            struct iovec local_region {
+                .iov_base = &local_int_buffer,
+                .iov_len = sizeof(local_int_buffer)
+            };
+
+            struct iovec remote_region {
+                .iov_base = (void*)current_addr,
+                .iov_len = sizeof(local_int_buffer)
+            };
+
+            ssize_t bytes_read = process_vm_readv(pid_int, &local_region, 1, &remote_region, 1, 0);
+
+            if(bytes_read == -1)
+            {
+                break;
+            }
+
+            if(local_int_buffer == value_to_find)
+            {
+                std::cout << "Found match at address: 0x" << std::hex << current_addr << std::dec << "\n";
+            }
+        }
+    }
 
 
+    close(target_fd);
     return 0;
 }
