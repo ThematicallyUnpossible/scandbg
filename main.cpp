3d0466acde35568638c4d3193da69caddea758d6
ThematicallyUnpossible <fernando_linux@proton.me>
2026-07-09 23:38:04 +0700

    utility header

--- main.cpp
+++ main.cpp
@@ -5,59 +5,11 @@
 #include <fcntl.h>
 #include <vector>
 #include <string>
-#include <limits>
 #include <optional>
-#include <type_traits>
 #include "core.h"
+#include "utility.h"
 
 #define STANDARD_OPERATION_SIZE 4096
-
-void clean_cin(){
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-}
-
-#define nl std::numeric_limits
-template<typename T>
-    requires std::is_arithmetic_v<T>
-void prompt_mutate_unified(T& x,std::string_view prefix,T minimum = nl<T>::min(),T maximum = nl<T>::max()){
-    std::cout << prefix;
-    T temporary{};
-    while(true){
-        std::cin >> temporary;
-        if(std::cin.fail()){
-            std::cout << prefix;
-            clean_cin();
-            continue;
-        }
-        if(temporary < minimum || temporary > maximum){
-            std::cout << prefix;
-            clean_cin();
-            continue;
-        }
-        break;
-    }
-    x = temporary;
-}
-
-void print_addresses(std::vector<ScannedObject>& list){
-
-    int print_count{};
-    std::cout << "\n--------------------------------------------\n";
-    for(const auto address : list){
-        std::cout << "0x" << std::hex << address.m_address << std::dec;
-        print_count += 1;
-
-        if(print_count < 3){
-            std::cout << " ";
-        }else{
-            std::cout << "\n";
-            print_count = 0;
-        }
-    }
-    std::cout << "\n--------------------------------------------\n";
-
-}
 
 const std::string G_action_list = "---------------------------\n"
                             "[1] scan new\n"
